caede0ef4a2c0b6b76e33309dd077d2d4ce15953
wataru4635 <wataemonn1014@gmail.com>
2023-07-29 18:22:13 +0900

    ピクパ以外コーディング完了

--- src/js/script.js
+++ src/js/script.js
@@ -82,3 +82,84 @@ var swiper = new Swiper(".js-campaign-swiper", {
     prevEl:'.campaign .swiper-button-prev',
   }
 });
+
+// トップへ戻るボタン//
+
+$(function () {
+  const pageTop = $(".to-top");
+  const footer = $(".footer");
+  const footerHeight = footer.innerHeight();
+  let distanceFromFooter;
+
+  pageTop.hide();
+  $(window).resize(function () {
+    // ウィンドウの幅に応じて余白を設定
+    const windowWidth = $(window).width();
+    distanceFromFooter = windowWidth < 768 ? 16 : 20; // SP（幅が768px未満）なら16、それ以外（PC）なら20の余白を設定
+  });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      pageTop.fadeIn();
+    } else {
+      pageTop.fadeOut();
+    }
+
+    const windowHeight = $(window).height();
+    const documentHeight = $(document).height();
+    const scrollPosition = $(this).scrollTop() + windowHeight;
+    const stopPosition = documentHeight - footerHeight - distanceFromFooter;
+
+    if (scrollPosition >= stopPosition) {
+      pageTop.css({
+        position: "absolute",
+        bottom: footerHeight + distanceFromFooter,
+      });
+    } else {
+      pageTop.css({
+        position: "fixed",
+        bottom: distanceFromFooter,
+      });
+    }
+  }).trigger('resize'); // 初回読み込み時にもウィンドウ幅を判別して余白を設定するようにします
+
+  pageTop.click(function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      500
+    );
+    return false;
+  });
+});
+
+// 画像のスライドアニメーション //
+
+//要素の取得とスピードの設定
+var box = $('.slide-animation'),
+    speed = 700;
+
+//.slide-animationの付いた全ての要素に対して下記の処理を行う
+box.each(function () {
+    $(this).append('<div class="color"></div>')
+    var color = $(this).find('.color'),
+        image = $(this).find('img');
+    var counter = 0;
+
+    image.css('opacity', '0');
+    color.css('width', '0%');
+    //inviewを使って背景色が画面に現れたら処理をする
+    color.on('inview', function () {
+        if (counter == 0) {
+            $(this).delay(200).animate({ 'width': '100%' }, speed, function () {
+                image.css('opacity', '1');
+                $(this).css({ 'left': '0', 'right': 'auto' });
+                $(this).animate({ 'width': '0%' }, speed);
+            })
+            counter = 1;
+        }
+    });
+});
+
+
